3c26ae75e3d8c5e4584bebbaed2984d09456c61e
agungmee <agunglab@gmail.com>
2023-08-03 08:23:03 +0700

    setup project

---
+++ db/db_connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "todolist";
+
+$connection = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+if ($connection) {
+    echo "";
+} else {
+    echo "Not Connect to Database";
+}
+
+?>
